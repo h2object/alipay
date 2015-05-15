dc3a1d596440aa6109f6a98fed67ea940ae63dc4
刘建平 <liujianping@h2object.io>
2015-05-15 16:36:00 +0800

    add notify implementions

--- invoice.go
+++ invoice.go
@@ -3,8 +3,11 @@ package alipay
 import (
 	"log"
 	"errors"
+	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strconv"
+	"strings"
 	"github.com/revel/revel"
 )
 
@@ -35,40 +38,44 @@ func Return(req *http.Request) (*Invoice, error) {
  	return nil, errors.New("trade status is not success or finished.")
 }
 
+func Notify(req *http.Request) (*Invoice, error) {
+	var invoice Invoice
+
+ 	log.Println("notify req method: %s", req.Request.Method)
+ 	log.Println("notify req: %s", req.Request.URL.String())
+
+ 	body, err := ioutil.ReadAll(req.Request.Body)
+ 	if err != nil {
+ 		return nil, err
+ 	}
+
+ 	queries, err := url.ParseQuery(string(body))
+ 	if err != nil {
+ 		return nil, err
+ 	}
+
+ 	trade_status := queries.Get("trade_status")
+ 	if trade_status == "TRADE_FINISHED" || trade_status == "TRADE_SUCCESS" {
+ 		invoice.OutTradeNo = queries.Get("out_trade_no")
+	 	invoice.TradeNo = queries.Get("trade_no")
+	 	invoice.BuyerID = queries.Get("buyer_id")
+	 	invoice.BuyerEmail = queries.Get("buyer_email")		
+		fee, _ := strconv.ParseFloat(queries.Get("total_fee"), 64)
+		invoice.TotalFee = fee
+		log.Println("notify invoice:", invoice)
+		return &invoice, nil
+ 	}
+
+ 	return nil, errors.New("trade status is not success or finished.")
+}
+
+
 func RevelReturn(req *revel.Request) (*Invoice, error) {
  	return Return(req.Request)
 }
 
-func Notify(req *http.Request) (*Invoice, error) {
-	var invoice Invoice
-
-	log.Println("notify req: %v", req)
-
- 	return &invoice, nil
-}
-
 func RevelNotify(req *revel.Request) (*Invoice, error) {
-	var invoice Invoice
-
- 	log.Println("revel notify req method: %s", req.Request.Method)
- 	log.Println("revel notify req: %s", req.Request.URL.String())
-
- 	// body, err := ioutil.ReadAll(req.Request.Body)
- 	// if err != nil {
- 	// 	return nil, err
- 	// }
-
- 	// log.Println("revel notify body: %s", string(body))
-
- 	if err := req.Request.ParseForm(); err != nil {
- 		return nil, err
- 	}
-
- 	log.Println("revel notify form: ", req.Request.Form)
- 	log.Println("revel notify post form: ", req.Request.PostForm)
-
-
- 	return &invoice, nil
+ 	return Notify(req.Request)
 }
 
 
